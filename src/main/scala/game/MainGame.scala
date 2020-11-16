78b8fc08508083122588e2c2c9e9062bc96c79aa
Jennifer Phan <j.phan@newmotion.com>
2020-11-16 19:52:30 +0100

    move snake and body according to direction

--- src/main/scala/game/MainGame.scala
+++ src/main/scala/game/MainGame.scala
@@ -1,8 +1,9 @@
 package game
 
 import org.scalajs.dom
-import dom.{document, html, CanvasRenderingContext2D}
+import dom.{CanvasRenderingContext2D, document, html}
 import html.Canvas
+import org.scalajs.dom.ext.KeyCode
 //import worm.Worm
 
 object MainGame {
@@ -10,10 +11,19 @@ object MainGame {
   val backgroundColor = "#409D42"
   val backgroundWall = "#022E03"
   var time = 0
-  val worm = new Worm(new WormCube(2, 1), Seq(new WormCube(1, 1)))
+  val worm = new Worm(new WormCube(6, 1), Seq(new WormCube(5, 1),new WormCube(4, 1), new WormCube(3, 1), new WormCube(2, 1), new WormCube(1, 1)).reverse)
 
   def main(args: Array[String]): Unit =  {
     dom.window.setInterval(drawCanvas _, 200)
+    document.onkeydown =
+      (e: dom.KeyboardEvent) => {
+        e.keyCode match {
+          case KeyCode.S => {worm.setDirection("down")}
+          case KeyCode.W => {worm.setDirection("up")}
+          case KeyCode.D => {worm.setDirection("right")}
+          case KeyCode.A => {worm.setDirection("left")}
+        }
+      }
   }
 
   def drawCanvas(): Unit = {
--- src/main/scala/game/Worm.scala
+++ src/main/scala/game/Worm.scala
@@ -1,6 +1,8 @@
 package game
 
-import org.scalajs.dom.CanvasRenderingContext2D
+import org.scalajs.dom
+import org.scalajs.dom.{CanvasRenderingContext2D, document}
+import org.scalajs.dom.ext.KeyCode
 
 class Worm(initHead: WormCube, initBody: Seq[WormCube]) {
   var head: WormCube = initHead
@@ -10,18 +12,31 @@ class Worm(initHead: WormCube, initBody: Seq[WormCube]) {
   def draw(ctx: CanvasRenderingContext2D): Unit = {
     head.draw(ctx)
     body.foreach(cube => cube.draw(ctx))
-
   }
 
   def move() = {
+    if (body.nonEmpty) {
+      body = body.drop(1)
+      body = body :+ new WormCube(head.getX(), head.getY())
+    }
     direction match {
       case "right" => {
         head = new WormCube(head.getX() + 1, head.getY())
-        if (body.length > 0) {
-          body = body.map(piece => new WormCube(piece.getX() + 1, piece.getY()))
-        }
+      }
+      case "left" => {
+        head = new WormCube(head.getX() - 1, head.getY())
+      }
+      case "down" => {
+        head = new WormCube(head.getX(), head.getY() + 1)
+      }
+      case "up" => {
+        head = new WormCube(head.getX(), head.getY() - 1)
       }
     }
+    }
+
+  def setDirection(dir: String) = {
+    direction = dir
   }
 
   def eat(): Unit = {
